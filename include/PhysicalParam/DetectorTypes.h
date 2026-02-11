9087cac8b0e40b936ee2eefb5978f69732661d2a
Jeppe Thingholm <jeppe.thingholm@fys.uio.no>
2026-02-11 11:03:33 +0100

    added qint detector type

--- include/PhysicalParam/DetectorTypes.h
+++ include/PhysicalParam/DetectorTypes.h
@@ -6,10 +6,12 @@
 #define DETECTORTYPES_H
 
 #define NUM_LABR_DETECTORS 12   //!< Number of LaBr detectors
+#define NUM_QINT_DETECTORS 1
 
 enum DetectorType {
     invalid,    //!< Invalid address
     labr,       //!< Is a labr detector
+    qint,
     any,        //!< Any detector
     unused      //!< Is a unused XIA channel
 };
