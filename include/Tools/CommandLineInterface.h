4bf4575a05d5ce376584ed1a341f18b0188a34df
Jeppe Thingholm <jkthingholm@gmail.com>
2026-04-07 10:49:45 +0200

    standard sort type coincidence to gap

--- include/Tools/CommandLineInterface.h
+++ include/Tools/CommandLineInterface.h
@@ -34,7 +34,7 @@ namespace CLI {
         std::optional<double> coincidenceTime = 100;
         std::optional<double> SplitTime = 1500;
         std::optional<bool> tree = false;
-        std::optional<sort_type> sortType = sort_type::coincidence;
+        std::optional<sort_type> sortType = sort_type::gap;
         std::optional<DetectorType> Trigger = DetectorType::labr;
     };
 
